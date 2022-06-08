94e7aa32c48e34b9c9b28ea466db733f797eff28
Prithivi-raj-sam <prithiviraj.stalinsundaraiya@chainsys.com>
2022-06-08 14:34:00 +0530

    interface concepts files cummitted

--- src/com/chainsys/bestPractices/clone/TestCloning.java
+++ src/com/chainsys/bestPractices/clone/TestCloning.java
@@ -5,12 +5,14 @@ public class TestCloning {
 		Student firstStudent=new Student();
 		firstStudent.name ="prithivi";
 		firstStudent.age=21;
-		System.out.println(firstStudent.hashCode());
+		System.out.println("first student:"+firstStudent.hashCode());
 		try {
+			Student thirdStudent=firstStudent;
 			Student secondStudent = (Student)firstStudent.clone();
 			System.out.println(secondStudent.name);
 			System.out.println(secondStudent.age);
-			System.out.println(secondStudent.hashCode());
+			System.out.println("third student:"+thirdStudent.hashCode());
+			System.out.println("second student:"+secondStudent.hashCode());
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
